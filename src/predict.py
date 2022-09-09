2ced5e2a356948652c62599ab8519d66aee12fac
SHIVAM PRASAD <shivam13juna@gmail.com>
2022-09-09 07:47:20 +0000

    COG is working, need to work on accuracy

--- src/predict.py
+++ src/predict.py
@@ -1,45 +1,15 @@
+from typing import Any
+from cog import BasePredictor, Input, Path
 import pyrootutils
-import os
-import torchvision.transforms as transforms
-import timm 
+import json
 import torch
-import os
+import timm
 import numpy as np
+import torchvision.transforms as transforms
+import os
+from timm.data.transforms_factory import transforms_imagenet_eval
+
 from PIL import Image
-from io import BytesIO
-import requests
-
-
-
-root = pyrootutils.setup_root(
-    search_from=os.getcwd(),
-    indicator=[".git", "pyproject.toml"],
-    pythonpath=True,
-    dotenv=True,
-)
-
-# ------------------------------------------------------------------------------------ #
-# `pyrootutils.setup_root(...)` is recommended at the top of each start file
-# to make the environment more robust and consistent
-#
-# the line above searches for ".git" or "pyproject.toml" in present and parent dirs
-# to determine the project root dir
-#
-# adds root dir to the PYTHONPATH (if `pythonpath=True`)
-# so this file can be run from any place without installing project as a package
-#
-# sets PROJECT_ROOT environment variable which is used in "configs/paths/default.yaml"
-# this makes all paths relative to the project root
-#
-# additionally loads environment variables from ".env" file (if `dotenv=True`)
-#
-# you can get away without using `pyrootutils.setup_root(...)` if you:
-# 1. move this file to the project root dir or install project as a package
-# 2. modify paths in "configs/paths/default.yaml" to not use PROJECT_ROOT
-# 3. always run this file from the project root dir
-#
-# https://github.com/ashleve/pyrootutils
-# ------------------------------------------------------------------------------------ #
 
 from typing import List, Tuple
 
@@ -52,194 +22,54 @@ from src import utils
 
 log = utils.get_pylogger(__name__)
 
-@utils.task_wrapper
-def predictor(cfg: DictConfig) -> Tuple[dict, dict]:
-    """Evaluates given checkpoint on a datamodule testset.
 
-    This method is wrapped in optional @task_wrapper decorator which applies extra utilities
-    before and after the call.
+root = pyrootutils.setup_root(
+    search_from=__file__,
+    indicator=[".git", "pyproject.toml"],
+    pythonpath=True,
+    dotenv=True,
+)
 
-    Args:
-        cfg (DictConfig): Configuration composed by Hydra.
+class Predictor(BasePredictor):
+    def setup(self):
+        """Load the model into memory to make running multiple predictions efficient"""
+        # self.model = timm.create_model('efficientnet_b3a', pretrained=True)
+        # self.model.eval()
+        # self.transform = transforms_imagenet_eval()
 
-    Returns:
-        Tuple[dict, dict]: Dict with metrics and dict with all instantiated objects.
-    """
+        # with open("imagenet_1k.json", "r") as f:
+        #     self.labels = list(json.load(f).values())
 
-    assert cfg.ckpt_path
+        self.model = timm.create_model('resnet18', pretrained=True, num_classes = 10)
+        dictt = torch.load('epoch_008.ckpt', map_location=torch.device('cpu'))
+        state_dict = {k.partition('net.')[2]: v for k,v in dictt['state_dict'].items()}
+        self.model.load_state_dict(state_dict)
+        self.model.eval()
 
-    log.info(f"Instantiating datamodule <{cfg.datamodule._target_}>")
-    datamodule: LightningDataModule = hydra.utils.instantiate(cfg.datamodule)
+    # Define the arguments and types the model takes as input
+    def predict(self, image: Path = Input(description="Image to classify")) -> Any:
+        """Run a single prediction on the model"""
 
-    log.info(f"Instantiating model <{cfg.model._target_}>")
-    model: LightningModule = hydra.utils.instantiate(cfg.model)
+        # Preprocess the image
+        img = Image.open(image)
 
-    log.info("Instantiating loggers...")
-    logger: List[LightningLoggerBase] = utils.instantiate_loggers(cfg.get("logger"))
+        transform = transforms.Compose([
+                        transforms.Resize(32),
+                        transforms.ToTensor()])
 
-    log.info(f"Instantiating trainer <{cfg.trainer._target_}>")
-    trainer: Trainer = hydra.utils.instantiate(cfg.trainer, logger=logger)
-
-    object_dict = {
-        "cfg": cfg,
-        "datamodule": datamodule,
-        "model": model,
-        "logger": logger,
-        "trainer": trainer,
-    }
-
-    if logger:
-        log.info("Logging hyperparameters!")
-        utils.log_hyperparameters(object_dict)
-
-    log.info("Starting testing!")
-    # trainer.test(model=model, datamodule=datamodule, ckpt_path=cfg.ckpt_path)
-
-    # for predictions use trainer.predict(...)
-    print("Getting response")
-    # response = requests.get(cfg.image)
-    # img = Image.open(BytesIO(response.content))
-    img = Image.open(cfg.image)
-    print("Got response")
-
-    print("Creating Transform")
-    # normalize = transforms.Normalize(mean=list(model.default_cfg['mean']),
-    #                                 std=list(model.default_cfg['std']))
+        img = transform(img)
+        img = img[np.newaxis, :]
 
 
+        # print("Predicting and shape of image: ", img.shape)
+        # predictions = trainer.predict(model=model, dataloaders=img, ckpt_path=cfg.ckpt_path)
+        with torch.no_grad():
+            predic = self.model(img)
+        label = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
+        val = np.argmax(predic, axis=1).numpy() - 1
 
-    # transform = transforms.Compose([
-    #                     transforms.Resize(256),
-    #                     transforms.CenterCrop(224),
-    #                     transforms.ToTensor(),
-    #                     normalize,
-    #                     ])
+        return label[int(val)]
 
-
-    transform = transforms.Compose([
-                    transforms.Resize(32),
-                    transforms.ToTensor()])
-
-    img = transform(img)
-
-    # img = img[-1]
-    # img = img[np.newaxis, :]
-    img = img[np.newaxis, :]
-
-
-    print("Predicting and shape of image: ", img.shape)
-    # predictions = trainer.predict(model=model, dataloaders=img, ckpt_path=cfg.ckpt_path)
-    model.eval()
-    with torch.no_grad():
-        predic = model(img)
-    label = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
-    val = np.argmax(predic, axis=1).numpy() - 1
-    print("val: ", val)
-    print("This is the prediction? : ", predic.shape, np.round(predic, 2), label[int(val)])
-
-    metric_dict = trainer.callback_metrics
-
-    return metric_dict, object_dict
-
-
-@utils.task_wrapper
-def predictor_cog(cfg: DictConfig, ) -> Tuple[dict, dict]:
-    """Evaluates given checkpoint on a datamodule testset.
-
-    This method is wrapped in optional @task_wrapper decorator which applies extra utilities
-    before and after the call.
-
-    Args:
-        cfg (DictConfig): Configuration composed by Hydra.
-
-    Returns:
-        Tuple[dict, dict]: Dict with metrics and dict with all instantiated objects.
-    """
-
-    assert cfg.ckpt_path
-
-    log.info(f"Instantiating datamodule <{cfg.datamodule._target_}>")
-    datamodule: LightningDataModule = hydra.utils.instantiate(cfg.datamodule)
-
-    log.info(f"Instantiating model <{cfg.model._target_}>")
-    model: LightningModule = hydra.utils.instantiate(cfg.model)
-
-    log.info("Instantiating loggers...")
-    logger: List[LightningLoggerBase] = utils.instantiate_loggers(cfg.get("logger"))
-
-    log.info(f"Instantiating trainer <{cfg.trainer._target_}>")
-    trainer: Trainer = hydra.utils.instantiate(cfg.trainer, logger=logger)
-
-    object_dict = {
-        "cfg": cfg,
-        "datamodule": datamodule,
-        "model": model,
-        "logger": logger,
-        "trainer": trainer,
-    }
-
-    if logger:
-        log.info("Logging hyperparameters!")
-        utils.log_hyperparameters(object_dict)
-
-    log.info("Starting testing!")
-    # trainer.test(model=model, datamodule=datamodule, ckpt_path=cfg.ckpt_path)
-
-    # for predictions use trainer.predict(...)
-    print("Getting response")
-    # response = requests.get(cfg.image)
-    # img = Image.open(BytesIO(response.content))
-    img = Image.open(cfg.image)
-    print("Got response")
-
-    print("Creating Transform")
-    # normalize = transforms.Normalize(mean=list(model.default_cfg['mean']),
-    #                                 std=list(model.default_cfg['std']))
-
-
-
-    # transform = transforms.Compose([
-    #                     transforms.Resize(256),
-    #                     transforms.CenterCrop(224),
-    #                     transforms.ToTensor(),
-    #                     normalize,
-    #                     ])
-
-
-    transform = transforms.Compose([
-                    transforms.Resize(32),
-                    transforms.ToTensor()])
-
-    img = transform(img)
-
-    # img = img[-1]
-    # img = img[np.newaxis, :]
-    img = img[np.newaxis, :]
-
-
-    print("Predicting and shape of image: ", img.shape)
-    # predictions = trainer.predict(model=model, dataloaders=img, ckpt_path=cfg.ckpt_path)
-    model.eval()
-    with torch.no_grad():
-        predic = model(img)
-    label = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
-    val = np.argmax(predic, axis=1).numpy() - 1
-    print("val: ", val)
-    print("This is the prediction? : ", predic.shape, np.round(predic, 2), label[int(val)])
-
-    metric_dict = trainer.callback_metrics
-
-    return metric_dict, object_dict
-
-
-
-@hydra.main(version_base="1.2", config_path=root / "configs", config_name="predict.yaml")
-def main(cfg: DictConfig) -> None:
-    predict(cfg)
-
-
-if __name__ == "__main__":
-    main()
 
 
 
--- src/predict_v1.py
+++ src/predict_v1.py
@@ -1,4 +1,15 @@
 import pyrootutils
+import os
+import torchvision.transforms as transforms
+import timm 
+import torch
+import os
+import numpy as np
+from PIL import Image
+from io import BytesIO
+import requests
+
+
 
 root = pyrootutils.setup_root(
     search_from=__file__,
@@ -6,8 +17,6 @@ root = pyrootutils.setup_root(
     pythonpath=True,
     dotenv=True,
 )
-import os
-import sys
 
 # ------------------------------------------------------------------------------------ #
 # `pyrootutils.setup_root(...)` is recommended at the top of each start file
@@ -32,23 +41,20 @@ import sys
 # https://github.com/ashleve/pyrootutils
 # ------------------------------------------------------------------------------------ #
 
-from typing import List, Optional, Tuple
+from typing import List, Tuple
 
 import hydra
-import pytorch_lightning as pl
 from omegaconf import DictConfig
-from pytorch_lightning import Callback, LightningDataModule, LightningModule, Trainer
+from pytorch_lightning import LightningDataModule, LightningModule, Trainer
 from pytorch_lightning.loggers import LightningLoggerBase
 
 from src import utils
 
 log = utils.get_pylogger(__name__)
 
-
 @utils.task_wrapper
-def train(cfg: DictConfig) -> Tuple[dict, dict]:
-    """Trains the model. Can additionally evaluate on a testset, using best weights obtained during
-    training.
+def predictor(cfg: DictConfig) -> Tuple[dict, dict]:
+    """Evaluates given checkpoint on a datamodule testset.
 
     This method is wrapped in optional @task_wrapper decorator which applies extra utilities
     before and after the call.
@@ -60,9 +66,7 @@ def train(cfg: DictConfig) -> Tuple[dict, dict]:
         Tuple[dict, dict]: Dict with metrics and dict with all instantiated objects.
     """
 
-    # set seed for random number generators in pytorch, numpy and python.random
-    if cfg.get("seed"):
-        pl.seed_everything(cfg.seed, workers=True)
+    assert cfg.ckpt_path
 
     log.info(f"Instantiating datamodule <{cfg.datamodule._target_}>")
     datamodule: LightningDataModule = hydra.utils.instantiate(cfg.datamodule)
@@ -70,20 +74,16 @@ def train(cfg: DictConfig) -> Tuple[dict, dict]:
     log.info(f"Instantiating model <{cfg.model._target_}>")
     model: LightningModule = hydra.utils.instantiate(cfg.model)
 
-    log.info("Instantiating callbacks...")
-    callbacks: List[Callback] = utils.instantiate_callbacks(cfg.get("callbacks"))
-
     log.info("Instantiating loggers...")
     logger: List[LightningLoggerBase] = utils.instantiate_loggers(cfg.get("logger"))
 
     log.info(f"Instantiating trainer <{cfg.trainer._target_}>")
-    trainer: Trainer = hydra.utils.instantiate(cfg.trainer, callbacks=callbacks, logger=logger)
+    trainer: Trainer = hydra.utils.instantiate(cfg.trainer, logger=logger)
 
     object_dict = {
         "cfg": cfg,
         "datamodule": datamodule,
         "model": model,
-        "callbacks": callbacks,
         "logger": logger,
         "trainer": trainer,
     }
@@ -92,43 +92,63 @@ def train(cfg: DictConfig) -> Tuple[dict, dict]:
         log.info("Logging hyperparameters!")
         utils.log_hyperparameters(object_dict)
 
-    if cfg.get("train"):
-        log.info("Starting training!")
-        trainer.fit(model=model, datamodule=datamodule, ckpt_path=cfg.get("ckpt_path"))
+    log.info("Starting testing!")
+    # trainer.test(model=model, datamodule=datamodule, ckpt_path=cfg.ckpt_path)
 
-    train_metrics = trainer.callback_metrics
+    # for predictions use trainer.predict(...)
+    print("Getting response")
+    # response = requests.get(cfg.image)
+    # img = Image.open(BytesIO(response.content))
+    img = Image.open(cfg.image)
+    print("Got response")
 
-    if cfg.get("test"):
-        log.info("Starting testing!")
-        ckpt_path = trainer.checkpoint_callback.best_model_path
-        if ckpt_path == "":
-            log.warning("Best ckpt not found! Using current weights for testing...")
-            ckpt_path = None
-        trainer.test(model=model, datamodule=datamodule, ckpt_path=ckpt_path)
-        log.info(f"Best ckpt path: {ckpt_path}")
+    print("Creating Transform")
+    # normalize = transforms.Normalize(mean=list(model.default_cfg['mean']),
+    #                                 std=list(model.default_cfg['std']))
 
-    test_metrics = trainer.callback_metrics
 
-    # merge train and test metrics
-    metric_dict = {**train_metrics, **test_metrics}
+
+    # transform = transforms.Compose([
+    #                     transforms.Resize(256),
+    #                     transforms.CenterCrop(224),
+    #                     transforms.ToTensor(),
+    #                     normalize,
+    #                     ])
+
+
+    transform = transforms.Compose([
+                    transforms.Resize(32),
+                    transforms.ToTensor()])
+
+    img = transform(img)
+
+    # img = img[-1]
+    # img = img[np.newaxis, :]
+    img = img[np.newaxis, :]
+
+
+    print("Predicting and shape of image: ", img.shape)
+    # predictions = trainer.predict(model=model, dataloaders=img, ckpt_path=cfg.ckpt_path)
+    model.eval()
+    with torch.no_grad():
+        predic = model(img)
+    label = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
+    val = np.argmax(predic, axis=1).numpy() - 1
+    print("val: ", val)
+    print("This is the prediction? : ", predic.shape, np.round(predic, 2), label[int(val)])
+
+    metric_dict = trainer.callback_metrics
 
     return metric_dict, object_dict
 
 
-@hydra.main(version_base="1.2", config_path=root / "configs", config_name="train.yaml")
-def main(cfg: DictConfig) -> Optional[float]:
-
-    # train the model
-    metric_dict, _ = train(cfg)
-
-    # safely retrieve metric value for hydra-based hyperparameter optimization
-    metric_value = utils.get_metric_value(
-        metric_dict=metric_dict, metric_name=cfg.get("optimized_metric")
-    )
-
-    # return optimized metric
-    return metric_value
+@hydra.main(version_base="1.2", config_path=root / "configs", config_name="predict.yaml")
+def main(cfg: DictConfig) -> None:
+    predict(cfg)
 
 
 if __name__ == "__main__":
     main()
+
+
+
