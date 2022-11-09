23d4a6046a94114157138b888b79c9183758b947
Shivam Prasad <shivam13juna@gmail.com>
2022-11-09 12:31:01 +0530

    Adding script file

--- src/train.py
+++ src/train.py
@@ -104,8 +104,7 @@ def train(cfg: DictConfig) -> Tuple[dict, dict]:
     log.info("Scripting Model")
 
     scripted_model = model.to_torchscript(
-        method="trace", example_inputs=torch.randn(1, 3, 32, 32)
-    )
+        method="script"    )
     torch.jit.save(scripted_model, f"{cfg.paths.output_dir}/model.script.pt")
     log.info(f"File size in MB: {os.path.getsize(f'{cfg.paths.output_dir}/model.script.pt') / 1e6}")
     log.info(f"Saving model to {cfg.paths.output_dir}")
